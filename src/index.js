976286fe69511e84878be2eab38fae3c77d03bf4
ajlevy <AJLevy125@icloud.com>
2024-12-21 09:42:20 -0500

    Mistake Included in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ChinesePalaceImage from "./pics/GoldenPalace.jpg";
+import ".styles.css";
 const image = document.createElement("img")
 image.src = ChinesePalaceImage
 document.body.appendChild(image)
+ContentDiv = document.querySelector("div#content")
