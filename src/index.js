2cb14351a42d35fbbd24c1b5bfa0e838b6ac4f06
ajlevy <AJLevy125@icloud.com>
2024-12-21 05:49:33 -0500

    small problems

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ChinesePalaceImage from ".pics/GoldenPalace.jpg";
+const image = document.createElement("img")
+image.src = ChinesePalaceImage
+document.body.appendChild(image)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
